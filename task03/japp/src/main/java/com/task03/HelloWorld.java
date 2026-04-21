bbcf479a306b23d011b19b1673a9e8cac3b7b708
Palak Goyal <palak_goyal@epam.com>
2026-04-21 14:01:12 +0530

    updated changes

--- task03/japp/src/main/java/com/task03/HelloWorld.java
+++ task03/japp/src/main/java/com/task03/HelloWorld.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,9 +20,28 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
 		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+		
+		try {
+			Map<String, Object> responseBody = new HashMap<String, Object>();
+			responseBody.put("statusCode", 200);
+			responseBody.put("message", "Hello from Lambda");
+			
+			Map<String, Object> resultMap = new HashMap<String, Object>();
+			resultMap.put("statusCode", 200);
+			resultMap.put("body", new ObjectMapper().writeValueAsString(responseBody));
+			resultMap.put("headers", new HashMap<String, String>() {{ put("Content-Type", "application/json"); }});
+			
+			return resultMap;
+		} catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+			e.printStackTrace();
+			
+			Map<String, Object> errorMap = new HashMap<String, Object>();
+			errorMap.put("statusCode", 500);
+			errorMap.put("body", "{\"statusCode\": 500, \"message\": \"Internal server error\"}");
+			return errorMap;
+		}
 	}
 }
+
+
